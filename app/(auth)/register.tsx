b50d4cc8803292b2581bf244ab1e642e8e6c2ad3
Dave Glad <mdaveglad@gmail.com>
2025-04-11 18:45:22 +0200

    feat(auth): add register screen

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,12 +1,34 @@
+import RegisterForm from '@/src/components/auth/register-form';
+import { ThemedView } from '@/src/components/themed-view';
+import { Center } from '@/src/components/ui/center';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import Animated, { FadeIn } from 'react-native-reanimated';
 
-const RegisterPage = () => {
+const RegisterScreen = () => {
    return (
-      <View>
-         <Text>RegisterPage</Text>
-      </View>
+      <ThemedView>
+         <SafeAreaView className="flex-1">
+            <Center className="justify-center my-6">
+               <Animated.Image
+                  entering={FadeIn.delay(300).duration(800)}
+                  style={[{ width: 100 }]}
+                  source={require('@/src/assets/images/logos/logo.png')}
+                  resizeMode="contain"
+                  accessibilityLabel="Delivery app logo"
+               />
+               <Animated.Text
+                  entering={FadeIn.delay(600).duration(800)}
+                  className="text-primary-500 text-2xl font-bold "
+                  style={{ letterSpacing: -0.3 }}
+               >
+                  Delivery
+               </Animated.Text>
+            </Center>
+            <RegisterForm />
+         </SafeAreaView>
+      </ThemedView>
    );
 };
 
-export default RegisterPage;
+export default RegisterScreen;
