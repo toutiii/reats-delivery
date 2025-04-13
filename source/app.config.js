a54c56af1100b0fa996a0d903ca61b498a51c421
tout'it <70380468+toutiii@users.noreply.github.com>
2025-04-13 11:08:05 +0200

    [BUILD] Starting over version count (#56)
    Co-authored-by: Toutel NTATLA <twotell75@gmail.com>

--- source/app.config.js
+++ source/app.config.js
@@ -3,7 +3,7 @@ export default ({ config }) => {
         ...config,
         name: "source",
         slug: "source",
-        version: "1.0.0",
+        version: "0.1.0",
         orientation: "portrait",
         icon: "./assets/icon.png",
         userInterfaceStyle: "light",
