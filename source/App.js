1b8737ad6e69602777ec35e615ba12ecacf46614
DaveGlad <mdaveglad@gmail.com>
2025-06-02 15:32:26 +0200

    feat(ui): add base UI components for the application

--- source/App.js
+++ source/App.js
@@ -11,6 +11,7 @@ import { AutocompleteDropdownContextProvider } from "react-native-autocomplete-d
 import SignupForm from "./legacy/forms/forms/SignupForm";
 import LoginForm from "./legacy/forms/forms/LoginForm";
 import OTPView from "./legacy/views/OTPView";
+import StartPage from "./screens/onboarding";
 
 const Stack = createStackNavigator();
 
@@ -21,7 +22,12 @@ export default class App extends Component {
                 <AutocompleteDropdownContextProvider>
                     <SafeAreaView style={{ flex: 1 }}>
                         <NavigationContainer>
-                            <Stack.Navigator initialRouteName="LoginForm">
+                            <Stack.Navigator initialRouteName="Onboarding">
+                                <Stack.Screen
+                                    name="Onboarding"
+                                    component={StartPage}
+                                    options={{ headerShown: false }}
+                                />
                                 <Stack.Screen
                                     name="MainDrawerNavigator"
                                     component={MainDrawerNavigator}
