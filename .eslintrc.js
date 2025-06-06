e319c5a7613e20ac1ecf11a17f4b98dd439e6387
DaveGlad <mdaveglad@gmail.com>
2025-06-06 12:45:03 +0200

    fix: update root ESLint config to work in CI environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,50 @@
 module.exports = {
     root: true,
-    extends: ["./source/.eslintrc.js"],
+    env: {
+        es2021: true,
+        node: true,
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+    extends: [
+        "eslint:recommended"
+    ],
+    overrides: [
+        {
+            files: [
+                "source/**/*.ts",
+                "source/**/*.tsx"
+            ],
+            parser: "@typescript-eslint/parser",
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module",
+                ecmaFeatures: {
+                    jsx: true,
+                },
+                project: "./source/tsconfig.json",
+            },
+            extends: [
+                "eslint:recommended",
+                "plugin:@typescript-eslint/recommended"
+            ],
+            rules: {
+                "react-hooks/exhaustive-deps": "off",
+                "@typescript-eslint/no-require-imports": "off"
+            }
+        }
+    ],
     parserOptions: {
-        project: "./source/tsconfig.json"
+        ecmaVersion: "latest",
+        sourceType: "module",
     },
     rules: {
-        "react-hooks/exhaustive-deps": "off",
-        "@typescript-eslint/no-require-imports": "off"
+        "indent": ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error", "double"],
+        "semi": ["error", "always"]
     }
 };
